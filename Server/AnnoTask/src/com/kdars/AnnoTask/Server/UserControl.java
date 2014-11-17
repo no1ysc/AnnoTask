03596eef178b9361d9e19c7da672b252dab938f4
drebong2` <drebong2`@KBDeskTop>
2014-11-17 02:12:45 -0500

    contentDB에서 DocID로  query

--- Server/AnnoTask/src/com/kdars/AnnoTask/Server/UserControl.java
+++ Server/AnnoTask/src/com/kdars/AnnoTask/Server/UserControl.java
@@ -102,45 +102,9 @@ public class UserControl extends Thread{
 		 */
 		
 		this.requestDocIDs = ContentDBManager.getInstance().getDocIDsFromDate(requestByDate.startDate, requestByDate.endDate, requestByDate.bNaver, requestByDate.bDaum, requestByDate.bNate);
-
-//		ArrayList<RequestByDate> requestedDocIDList = new ArrayList<RequestByDate>();
-//		Statement stmt = null;
-//		ResultSet rs = null;
-//		String sql;
-		
-//		try {
-//			sql = "select DocID from ContentDB where newsDate >= ('startDate') AND newsDate <= ('endDate')";
-//			
-//			//stmt = conn.createStatment();
-//			rs = stmt.executeQuery(sql);
-//			requestedDocIDList.add(rs);
-//			requestedDocIDList.get(rs);
-//			
-//			while(rs.next()) {
-//				getRowCount();
-//			}
-//			
-//		}catch (Exception e) {
-//			System.out.println("requestByDateHandler exception: " + e.toString());
-//		}
-//		
-//		stmt.execute(sql);
-//		requestedDocIDList.add(stmt.execute(sql));
-//		requestedDocIDList.get(stmt.execute(sql));
-//		requestedDocIDList.add(ContentDBManager.getInstance().getContent(docID));
-//		requestedDocIDList.add(int docID, sql);
-		
-	}
-		public int getRowCount() {
-			return requestedDocIDList.size();
-		}
-		
-		
-		
-		
 		
 		SendDocumentCount sendDocumentCount = new SendDocumentCount();
-		sendDocumentCount.doucumentCount = 100;
+		sendDocumentCount.doucumentCount = requestDocIDs.size();
 		
 		String json = new JSONSerializer().exclude("*.class").serialize(sendDocumentCount);
 		System.out.println(json);
@@ -150,7 +114,8 @@ public class UserControl extends Thread{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+}
+
 
 	private String commandFromUser() {
 		String command = "";
