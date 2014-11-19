91182a416c42ec60da049b11bbc7555dc6467c4f
Jin <Jin@Jin-PC>
2014-11-19 14:32:35 -0500

    deletelistdb still revising

--- Server/AnnoTask/src/com/kdars/AnnoTask/DB/DeleteListDBManager.java
+++ Server/AnnoTask/src/com/kdars/AnnoTask/DB/DeleteListDBManager.java
@@ -17,11 +17,23 @@ public class DeleteListDBManager {
 	 * @param from 
 	 * @return to
 	 */
-	public String queryDeleteTerm(String deleteTerm){
-		// TODO : from을 날려서 to가 올라옴.
-		return deleteListDB.query(deleteTerm);
+	public boolean CheckForDeleteTerm(String deleteTerm){
+		// TODO : term 날려서 term이 deletelistDB에 있으면 true, 없으면 false
+		if (deleteListDB.query(deleteTerm) == null){
+			return false;
+		}
+		
+		return true;
 	}
 	
-	public boolean setDeleteTermEntry(String DeleteCandidate,)
+	public boolean AddTermToDelete(String deleteTerm){
+		// TODO : term을 성공적으로 deletelistDB에 add했다면 true, 실패했다면 false
+		return deleteListDB.add(deleteTerm);
+	}
+	
+	public boolean RemoveTermFromDeleteList(String deleteTerm){
+		// TODO : term을 성공적으로 deletelistDB에서부터 remove했다면 true, 실패했다면 false
+		return deleteListDB.add(deleteTerm);
+	}
 	
 }
