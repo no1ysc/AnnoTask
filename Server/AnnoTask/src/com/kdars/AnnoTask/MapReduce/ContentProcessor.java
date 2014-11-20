1c3b3828c7e6cda41d7293b25c8e92589cb136d3
kihpark <kihpark@KHP-PC>
2014-11-19 23:00:30 -0500

    Re commit by kihpark

--- Server/AnnoTask/src/com/kdars/AnnoTask/MapReduce/ContentProcessor.java
+++ Server/AnnoTask/src/com/kdars/AnnoTask/MapReduce/ContentProcessor.java
@@ -11,7 +11,7 @@ import com.kdars.AnnoTask.DB.TermFreqDBManager;
 
 public class ContentProcessor extends Thread{
 //	private NgramFilter ngramFilter;	// TODO: 저쪽으로 가야할듯., 유저가 가저가기 전,,,,,,에서 하는걸로,.
-	private int nGram;
+	private int nGram = GlobalContext.getInstance().getN_Gram();
 	private Document document = null;
 	
 	public enum ProcessState{
@@ -34,6 +34,7 @@ public class ContentProcessor extends Thread{
 		DuplicationChecker dupChecker = new DuplicationChecker();
 		StopWordRemover stopWordRemover = new StopWordRemover();
 	
+		extractTermStructure(tokenizer, dupChecker, stopWordRemover, document);
 //		for (int docID = this.startDocIDIndex; docID <= this.endDocIDIndex; docID++){
 //			Document document = ContentDBManager.getInstance().getContent(docID);
 //			
@@ -54,7 +55,7 @@ public class ContentProcessor extends Thread{
 				for (Iterator<Map.Entry<String, Integer>> iter = docByTermList[i].entrySet().iterator(); iter.hasNext();){
 					Map.Entry<String, Integer> entry = iter.next();
 					String keyCheck = entry.getKey();
-					if (stopWordRemover.isStopWord(keyCheck) || dupChecker.duplicationCheck(keyCheck)){
+					if (stopWordRemover.isStopWord(keyCheck)){ // || dupChecker.duplicationCheck(keyCheck)){
 						iter.remove();
 						continue;
 					}
