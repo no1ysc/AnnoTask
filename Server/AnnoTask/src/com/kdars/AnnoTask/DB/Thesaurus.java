2d989b4686bf34dda7183dab2137c767a709014c
Jin <Jin@Jin-PC>
2014-12-04 19:52:19 -0500

    Thesaurus DB add method complete Thesaurus class get & set methods added

--- Server/AnnoTask/src/com/kdars/AnnoTask/DB/Thesaurus.java
+++ Server/AnnoTask/src/com/kdars/AnnoTask/DB/Thesaurus.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 public class Thesaurus {
 	private int conceptFromID;
-	private String conceptFrom = "temporary";
+	private String conceptFrom;
 	private int conceptToID;
 	private String conceptTo;
 	private String metaOntology;
@@ -17,9 +17,72 @@ public class Thesaurus {
 	public Thesaurus(){
 	}
 	
+	public void setConceptFromID(int conceptfromid) {
+		conceptFromID = conceptfromid;
+	}
+
+	public void setConceptFrom(String conceptfrom) {
+		conceptFrom = conceptfrom;
+	}
+	
+	public void setConceptToID(int concepttoid) {
+		conceptToID = concepttoid;
+	}
+	
+	public void setConceptTo(String conceptto) {
+		conceptTo = conceptto;
+	}
+	
+	public void setMetaOntology(String metaontology) {
+		metaOntology = metaontology;
+	}
+	
+	public void setMetaName(String metaname) {
+		metaName = metaname;
+	}
+	
+	public void setPOS(String pos) {
+		partOfSpeech = pos;
+	}
+	
+	public void setDefinition( String definition) {
+		definition = definition;
+	}
+	
+	public int getConceptFromID() {
+		return this.conceptFromID;
+	}
+
 	public String getConceptFrom() {
 		return this.conceptFrom;
 	}
+	
+	public int getConceptToID() {
+		return this.conceptToID;
+	}
+	
+	public String getConceptTo() {
+		return this.conceptTo;
+	}
+	
+	public String getMetaOntology() {
+		return this.metaOntology;
+	}
+	
+	public String getMetaName() {
+		return this.metaName;
+	}
+	
+	public String getPOS() {
+		return this.partOfSpeech;
+	}
+	
+	public String getDefinition() {
+		return this.definition;
+	}
+//	public String getConceptFrom() {
+//		return this.conceptFrom;
+//	}
 	
 	
 }
--- Server/AnnoTask/src/com/kdars/AnnoTask/DB/ThesaurusDBConnector.java
+++ Server/AnnoTask/src/com/kdars/AnnoTask/DB/ThesaurusDBConnector.java
@@ -10,8 +10,19 @@ import com.kdars.AnnoTask.ContextConfig;
 import com.mysql.jdbc.Connection;
 
 public class ThesaurusDBConnector {
-	private String conceptFromTable = ContextConfig.getInstance().Thesaurus_DB_TABLE_NAME1;
-	private String conceptToTable = ContextConfig.getInstance().Thesaurus_DB_TABLE_NAME2;
+//	private String conceptFromTable = ContextConfig.getInstance().Thesaurus_DB_TABLE_NAME1;
+//	private String conceptToTable = ContextConfig.getInstance().Thesaurus_DB_TABLE_NAME2;
+	private String conceptFromTable = "conceptfromtable_test";
+	private String conceptToTable = "concepttotable_test";
+	private String fromColName1 = "ConceptFrom_ID";
+	private String fromColName2 = "ConceptFrom";
+	private String foreignKeyColName = "ConceptTo_ID";
+	private String toColName1 = "ConceptTo";
+	private String toColName2 = "MetaOntology";
+	private String toColName3 = "MetaName";
+	private String toColName4 = "POS";
+	private String toColName5 = "Definition";
+	
 	private java.sql.Connection sqlConnection;
 	// TODO : 향후 한번에 조절하기 위해 모아야할 정보 : SQL 커넥션 정보,
 	
@@ -26,7 +37,39 @@ public class ThesaurusDBConnector {
 	}
 	
 	public boolean add(Thesaurus thes){
-		return false;
+
+		try {
+			java.sql.Statement stmt = sqlConnection.createStatement();
+			String conceptToEscape = escape(thes.getConceptTo());
+			String conceptFromEscape = escape(thes.getConceptFrom());
+			String conceptToInsertQuery = "insert into " + conceptToTable + " (" + toColName1 + ", " + toColName2 + ", " + toColName3 + ") values (\"" + conceptToEscape + "\", '" + thes.getMetaOntology() + "', '" + thes.getMetaOntology() + "');";
+			String conceptToIDgetQuery = "select " + foreignKeyColName + " from " + conceptToTable + " where " + toColName1 + " = \"" + conceptToEscape + "\";";
+			
+			ResultSet checkConceptToID = stmt.executeQuery(conceptToIDgetQuery);
+			if (!checkConceptToID.next()){
+				stmt.executeUpdate(conceptToInsertQuery);
+				ResultSet getConceptToID = stmt.executeQuery(conceptToIDgetQuery);
+				getConceptToID.next();
+				thes.setConceptToID(getConceptToID.getInt(1));
+				String conceptFromInsertQuery = "insert into " + conceptFromTable + " (" + fromColName2 + ", " + foreignKeyColName + ") values (\"" + conceptFromEscape + "\", '" + String.valueOf(thes.getConceptToID()) + "');";
+				stmt.executeUpdate(conceptFromInsertQuery);
+				stmt.close();
+				return true;
+			}
+			
+			thes.setConceptToID(checkConceptToID.getInt(1));
+			String conceptFromInsertQuery = "insert into " + conceptFromTable + " (" + fromColName2 + ", " + foreignKeyColName + ") values (\"" + conceptFromEscape + "\", '" + String.valueOf(thes.getConceptToID()) + "');";
+			stmt.executeUpdate(conceptFromInsertQuery);
+			stmt.close();
+			
+//			disconnect(sqlConnectionLocal);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+//			disconnect(sqlConnectionLocal);
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean delete(Thesaurus thes){  
@@ -34,20 +77,30 @@ public class ThesaurusDBConnector {
 	}
 	
 	public Thesaurus query(String colName, String value){
+		// TODO checks for duplicates in ThesaurusDB.  Null if there is no duplicate. Thesaurus containing a row in ThesaurusDB if there is a duplicate.
 //		java.sql.Connection sqlConnectionLocal = connect();
 		Thesaurus conceptFromTermCheck = new Thesaurus();
 		ResultSet resultSet = null;
 		try {
 			java.sql.Statement stmt = sqlConnection.createStatement();
 			String valueEscape = escape(value);
-			resultSet = stmt.executeQuery("select * from " + conceptFromTable + " where " + colName + " = \"" + valueEscape + "\";");
+			resultSet = stmt.executeQuery("select * from " + conceptFromTable + ", " + conceptToTable + " where " + colName + " = \"" + valueEscape + "\" and " + conceptFromTable + "." + foreignKeyColName + " = " + conceptToTable + "." + foreignKeyColName + ";");
 			/* exist check */
 			if(!resultSet.next()){
 				stmt.close();
 //				disconnect(sqlConnectionLocal);
 				return null;
 			}
-			conceptFromTermCheck.getConceptFrom();
+
+			conceptFromTermCheck.setConceptFromID(resultSet.getInt(1));
+			conceptFromTermCheck.setConceptFrom(resultSet.getString(2));
+			conceptFromTermCheck.setConceptToID(resultSet.getInt(3));
+			conceptFromTermCheck.setConceptTo(resultSet.getString(5));
+			conceptFromTermCheck.setMetaOntology(resultSet.getString(6));
+			conceptFromTermCheck.setMetaName(resultSet.getString(7));
+			conceptFromTermCheck.setPOS(resultSet.getString(8));
+			conceptFromTermCheck.setDefinition(resultSet.getString(9));
+			
 			stmt.close();
 //			disconnect(sqlConnectionLocal);
 		} catch (SQLException e) {
