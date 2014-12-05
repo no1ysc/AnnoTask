1f2637f372eaae6d150fa00656b1a916c17c7e13
kihpark <kihpark85@gmail.com>
2014-12-04 21:45:46 -0500

    add event on button2

--- Client/AnnoTaskClient/AnnoTaskClient/MainWindow.Designer.cs
+++ Client/AnnoTaskClient/AnnoTaskClient/MainWindow.Designer.cs
@@ -199,7 +199,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(193, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 12;
             this.label2.Text = "끝나는 날짜";
             // 
@@ -208,7 +208,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(11, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 11;
             this.label1.Text = "시작 날짜";
             // 
@@ -241,7 +241,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(699, 77);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.Size = new System.Drawing.Size(61, 13);
             this.label7.TabIndex = 21;
             this.label7.Text = "총 문서수:";
             // 
@@ -259,7 +259,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(825, 77);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 19;
             this.label4.Text = "총 단어수:";
             // 
@@ -393,7 +393,7 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label3.Location = new System.Drawing.Point(4, 4);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.Size = new System.Drawing.Size(72, 15);
             this.label3.TabIndex = 0;
             this.label3.Text = "단어리스트";
             // 
@@ -432,7 +432,7 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label5.Location = new System.Drawing.Point(3, 4);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.Size = new System.Drawing.Size(72, 15);
             this.label5.TabIndex = 0;
             this.label5.Text = "문서리스트";
             // 
@@ -456,7 +456,7 @@
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label6.Location = new System.Drawing.Point(3, 4);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.Size = new System.Drawing.Size(59, 15);
             this.label6.TabIndex = 0;
             this.label6.Text = "기사전문";
             // 
@@ -473,7 +473,7 @@
             this.tabPage3.Controls.Add(this.vScrollBar5);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(944, 417);
+            this.tabPage3.Size = new System.Drawing.Size(944, 414);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "2-gram";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -538,7 +538,7 @@
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label8.Location = new System.Drawing.Point(4, 4);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(67, 15);
+            this.label8.Size = new System.Drawing.Size(72, 15);
             this.label8.TabIndex = 0;
             this.label8.Text = "단어리스트";
             // 
@@ -577,7 +577,7 @@
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label9.Location = new System.Drawing.Point(3, 4);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(67, 15);
+            this.label9.Size = new System.Drawing.Size(72, 15);
             this.label9.TabIndex = 0;
             this.label9.Text = "문서리스트";
             // 
@@ -601,7 +601,7 @@
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label10.Location = new System.Drawing.Point(3, 4);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(55, 15);
+            this.label10.Size = new System.Drawing.Size(59, 15);
             this.label10.TabIndex = 0;
             this.label10.Text = "기사전문";
             // 
@@ -618,7 +618,7 @@
             this.tabPage4.Controls.Add(this.vScrollBar9);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(944, 417);
+            this.tabPage4.Size = new System.Drawing.Size(944, 414);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "3-gram";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -683,7 +683,7 @@
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label11.Location = new System.Drawing.Point(4, 4);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(67, 15);
+            this.label11.Size = new System.Drawing.Size(72, 15);
             this.label11.TabIndex = 0;
             this.label11.Text = "단어리스트";
             // 
@@ -722,7 +722,7 @@
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label12.Location = new System.Drawing.Point(3, 4);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(67, 15);
+            this.label12.Size = new System.Drawing.Size(72, 15);
             this.label12.TabIndex = 0;
             this.label12.Text = "문서리스트";
             // 
@@ -746,7 +746,7 @@
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label13.Location = new System.Drawing.Point(3, 4);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(55, 15);
+            this.label13.Size = new System.Drawing.Size(59, 15);
             this.label13.TabIndex = 0;
             this.label13.Text = "기사전문";
             // 
@@ -764,7 +764,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(944, 417);
+            this.tabPage1.Size = new System.Drawing.Size(944, 414);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "4-gram";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -829,7 +829,7 @@
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label14.Location = new System.Drawing.Point(4, 4);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(67, 15);
+            this.label14.Size = new System.Drawing.Size(72, 15);
             this.label14.TabIndex = 0;
             this.label14.Text = "단어리스트";
             // 
@@ -868,7 +868,7 @@
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label15.Location = new System.Drawing.Point(3, 4);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(67, 15);
+            this.label15.Size = new System.Drawing.Size(72, 15);
             this.label15.TabIndex = 0;
             this.label15.Text = "문서리스트";
             // 
@@ -892,7 +892,7 @@
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label16.Location = new System.Drawing.Point(3, 4);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(55, 15);
+            this.label16.Size = new System.Drawing.Size(59, 15);
             this.label16.TabIndex = 0;
             this.label16.Text = "기사전문";
             // 
@@ -928,6 +928,7 @@
             this.button2.TabIndex = 26;
             this.button2.Text = "불용어 추가";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // MainWindow
             // 
