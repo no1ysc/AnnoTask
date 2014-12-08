b319a09e2ed290b0ee9b9e8992e47df81b94788b
drebong2` <drebong2`@KBDeskTop>
2014-12-07 20:20:42 -0500

    Fixed size window and add items on ComboBox

--- Client/AnnoTaskClient/AnnoTaskClient/AddThesaurusWindow.Designer.cs
+++ Client/AnnoTaskClient/AnnoTaskClient/AddThesaurusWindow.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.ConceptFromComboBox = new System.Windows.Forms.ComboBox();
+            this.ConceptToComboBox = new System.Windows.Forms.ComboBox();
+            this.MetaOntologyComboBox = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.addThesaurusButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,9 +43,6 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -50,29 +50,39 @@
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // ConceptFromComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(38, 45);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(206, 21);
-            this.comboBox1.TabIndex = 0;
+            this.ConceptFromComboBox.FormattingEnabled = true;
+            this.ConceptFromComboBox.Location = new System.Drawing.Point(38, 45);
+            this.ConceptFromComboBox.Name = "ConceptFromComboBox";
+            this.ConceptFromComboBox.Size = new System.Drawing.Size(206, 21);
+            this.ConceptFromComboBox.TabIndex = 0;
             // 
-            // comboBox2
+            // ConceptToComboBox
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(38, 123);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(206, 21);
-            this.comboBox2.TabIndex = 1;
+            this.ConceptToComboBox.FormattingEnabled = true;
+            this.ConceptToComboBox.Location = new System.Drawing.Point(38, 123);
+            this.ConceptToComboBox.Name = "ConceptToComboBox";
+            this.ConceptToComboBox.Size = new System.Drawing.Size(206, 21);
+            this.ConceptToComboBox.TabIndex = 1;
             // 
-            // comboBox3
+            // MetaOntologyComboBox
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(263, 122);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(200, 21);
-            this.comboBox3.TabIndex = 2;
+            this.MetaOntologyComboBox.FormattingEnabled = true;
+            this.MetaOntologyComboBox.Items.AddRange(new object[] {
+            "Agent",
+            "Knowledge",
+            "Event",
+            "Task",
+            "Resource",
+            "Location",
+            "Organization",
+            "Action",
+            "Unknown"});
+            this.MetaOntologyComboBox.Location = new System.Drawing.Point(263, 122);
+            this.MetaOntologyComboBox.Name = "MetaOntologyComboBox";
+            this.MetaOntologyComboBox.Size = new System.Drawing.Size(200, 21);
+            this.MetaOntologyComboBox.TabIndex = 2;
             // 
             // panel1
             // 
@@ -82,6 +92,14 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(206, 210);
             this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 203);
+            this.textBox1.TabIndex = 0;
             // 
             // splitContainer1
             // 
@@ -100,6 +118,23 @@
             this.splitContainer1.Size = new System.Drawing.Size(477, 210);
             this.splitContainer1.SplitterDistance = 203;
             this.splitContainer1.TabIndex = 4;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(4, 4);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(196, 203);
+            this.treeView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(264, 203);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // addThesaurusButton
             // 
@@ -171,29 +206,6 @@
             this.label6.TabIndex = 11;
             this.label6.Text = "기사전문";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 4);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(203, 203);
-            this.textBox1.TabIndex = 0;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 4);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(264, 203);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(4, 4);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(196, 203);
-            this.treeView1.TabIndex = 0;
-            // 
             // AddThesaurusWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -208,11 +220,13 @@
             this.Controls.Add(this.addThesaurusButton);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.MetaOntologyComboBox);
+            this.Controls.Add(this.ConceptToComboBox);
+            this.Controls.Add(this.ConceptFromComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "AddThesaurusWindow";
-            this.Text = "AddThesaurusWindow";
+            this.Text = "Add Thesaurus Window";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -226,9 +240,9 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox ConceptFromComboBox;
+        private System.Windows.Forms.ComboBox ConceptToComboBox;
+        private System.Windows.Forms.ComboBox MetaOntologyComboBox;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.SplitContainer splitContainer1;
