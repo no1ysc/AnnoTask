0427ce907c6fc39cd8a5e42017866d1436026b69
kihpark <kihpark85@gmail.com>
2014-12-08 16:03:03 -0500

    add LinkedList.cs file to project.

---
+++ Client/AnnoTaskClient/AnnoTaskClient/Logic/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoTaskClient.Logic
+{
+    class LinkedList
+    {
+        private string linkedTerm;
+        private string metaInfo;
+
+        public string linkedTerms
+        {
+            get { return linkedTerm; }
+            set { linkedTerm = value; }
+        }
+
+        public string metaInfos
+        {
+            get { return metaInfo; }
+            set { metaInfo = value; }
+        }
+    }
+}
