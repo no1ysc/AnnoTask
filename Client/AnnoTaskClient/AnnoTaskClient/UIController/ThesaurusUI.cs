b29e42dbc8c604f0c6f3d0d632e2cf35622f8135
kihpark <kihpark85@gmail.com>
2014-12-07 20:06:14 -0500

    add ThesaurusUI

---
+++ Client/AnnoTaskClient/AnnoTaskClient/UIController/ThesaurusUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnnoTaskClient.UIController
+{
+    class ThesaurusUI
+    {
+        private AddThesaurusWindow addThesaurusWindow;
+
+        public ThesaurusUI(AddThesaurusWindow addThesaurusWindow)
+		{
+            this.addThesaurusWindow = addThesaurusWindow;
+		}
+
+        public String conceptFrom
+        {
+            get { return (String)addThesaurusWindow.comboBox1.SelectedValue; }
+        }
+
+        public String conceptTo
+        {
+            get { return (String)addThesaurusWindow.comboBox2.SelectedValue; }
+        }
+
+        public String metaOntology
+        {
+            get { return (String)addThesaurusWindow.comboBox3.SelectedValue; }
+        }
+
+    }
+}
