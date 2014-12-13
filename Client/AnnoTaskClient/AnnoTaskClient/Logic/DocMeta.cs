2100f1b3530fb773f0d4f56b385c3b00973927fa
kihpark <kihpark85@gmail.com>
2014-12-13 18:43:34 -0500

    add DocMeta.cs

---
+++ Client/AnnoTaskClient/AnnoTaskClient/Logic/DocMeta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnnoTaskClient.Logic
+{
+    public class DocMeta
+    {
+        public DocMeta(List<string> category, List<int> doc_id, List<string> title)
+		{
+			this.category = category;
+            this.docIDList = doc_id;
+            this.title = title;
+		}
+        
+        private List<string> category;
+        public List<string> Category
+		{
+			get { return category; }
+			set { category = value; }
+		}
+
+        private List<int> docIDList;
+        public List<int> DocIDList
+		{
+			get { return docIDList; }
+			set { docIDList = value; }
+		}
+        
+        private List<string> title;
+        public List<string> Title
+		{
+			get { return title; }
+			set { title = value; }
+		}
+    }
+}
