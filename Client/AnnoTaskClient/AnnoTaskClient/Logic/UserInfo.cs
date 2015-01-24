d45820ba8a47fb3a2b17c0ff465f2016ecb2ec65
kihpark <kihpark85@gmail.com>
2015-01-24 05:34:49 -0500

    UserInfo 클래스 파일 추가

---
+++ Client/AnnoTaskClient/AnnoTaskClient/Logic/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoTaskClient.Logic
+{
+    class UserInfo
+    {
+        public string userId;
+        public string userName;
+        public bool isLoginSuccess;
+    }
+}
